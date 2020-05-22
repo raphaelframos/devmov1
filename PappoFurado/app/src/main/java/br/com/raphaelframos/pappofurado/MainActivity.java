d55fbf100249d53eb05597c01ef47015298c0414
Raphael Ramos <raphaelframos@gmail.com>
2020-05-22 20:44:22 -0300

    Mostrando lista de canais

--- PappoFurado/app/src/main/java/br/com/raphaelframos/pappofurado/MainActivity.java
+++ PappoFurado/app/src/main/java/br/com/raphaelframos/pappofurado/MainActivity.java
@@ -1,14 +1,43 @@
 package br.com.raphaelframos.pappofurado;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+import br.com.raphaelframos.pappofurado.model.Canal;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextNomeDoCanal;
+    private Spinner spinnerCategoria;
+    private ImageView imageViewCriar;
+    private ListView listViewCanais;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vincularViewPeloId();
+        ArrayList<Canal> canais = new ArrayList<>();
+        canais.add(new Canal("STF", "Política"));
+        canais.add(new Canal("Flamengo", "Esporte"));
+        ArrayAdapter<Canal> adapter = new ArrayAdapter<Canal>(this, android.R.layout.simple_list_item_1, canais);
+        listViewCanais.setAdapter(adapter);
+
+    }
+
+    private void vincularViewPeloId(){
+        editTextNomeDoCanal = findViewById(R.id.editText_nome_canal);
+        spinnerCategoria = findViewById(R.id.spinner_categorias);
+        imageViewCriar = findViewById(R.id.imageView_criar);
+        listViewCanais = findViewById(R.id.listView_canais);
     }
 }
---
+++ PappoFurado/app/src/main/java/br/com/raphaelframos/pappofurado/model/Canal.java
@@ -0,0 +1,47 @@
+package br.com.raphaelframos.pappofurado.model;
+
+import androidx.annotation.NonNull;
+
+public class Canal {
+
+    private String id;
+    private String nome;
+    private String categoria;
+
+    public Canal(){}
+
+    public Canal(String nome, String categoria) {
+        setCategoria(categoria);
+        setNome(nome);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return getNome() + " - " + getCategoria();
+    }
+}
